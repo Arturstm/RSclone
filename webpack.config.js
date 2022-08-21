94007a4cb81089144917e678cb642fb7b2339d24
Elf <Elf@Elf.local>
2022-08-21 18:26:22 +0400

    fix: add rool for css loader in webpack.json

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,10 @@ const baseConfig = {
   module: {
     rules: [
       {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
         test: /\.scss$/i,
         use: ['style-loader', 'css-loader', "sass-loader"],
       },
