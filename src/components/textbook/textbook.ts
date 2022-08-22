8625f5871907f72aba71a044e812545a4013a87d
Elf <Elf@Elf.local>
2022-08-23 00:40:35 +0400

    feat: add saving page and group in localStorage

--- src/components/textbook/textbook.ts
+++ src/components/textbook/textbook.ts
@@ -2,6 +2,10 @@ import './textbook.css';
 const url = 'https://rs-lang-179.herokuapp.com/';
 let group = 0;
 let page = 0;
+let currentGroup: number;
+let currentPage: number;
+let currentPageNum: number;
+const pageNumber = document.querySelector('.page-number') as HTMLElement;
 interface ResponseItem {
   id: string;
   group: number;
@@ -24,17 +28,17 @@ const prev = document.querySelector('#prev');
 
 const groupInputs = document.querySelector('.group-inputs') as HTMLElement;
 
-async function fetchData() {
+async function fetchData(p: number, g: number) {
   try {
-    const response: Response = await fetch(`${url}words?group=${group}&page=${page}`);
+    const response: Response = await fetch(`${url}words?group=${g}&page=${p}`);
     return await response.json();
   } catch (error) {
     console.log(error);
   }
 }
 
-async function reRenderData() {
-  const data = await fetchData();
+async function reRenderData(p: number, g: number) {
+  const data = await fetchData(p, g);
   (document.querySelector('.content') as HTMLElement).innerHTML = '';
   data.forEach((item: ResponseItem) => {
     const card = document.createElement('div');
@@ -126,34 +130,58 @@ async function reRenderData() {
 groupInputs.onclick = function (event: Event) {
   const target = event.target;
   group = Number((target as HTMLInputElement).id);
-  reRenderData();
+  localStorage.setItem('currentGroup', `${group}`);
+  reRenderData(page, group);
 };
 
-function nextPage() {
+function nextPage(p: number) {
+  page = p;
   if (page < 29) {
     (prev as HTMLElement).style.display = 'inline';
     page++;
+    pageNumber.innerHTML = (page + 1).toString();
+    localStorage.setItem('currentPage', `${page}`);
+    localStorage.setItem('currentPageNum', `${page + 1}`);
   } else {
     (next as HTMLElement).style.display = 'none';
   }
-  reRenderData();
+  reRenderData(page, group);
 }
 
-function prevPage() {
+function prevPage(p: number) {
+  page = p;
   if (page > 0) {
     page--;
     (next as HTMLElement).style.display = '';
+    pageNumber.innerHTML = (page + 1).toString();
+    localStorage.setItem('currentPage', `${page}`);
+    localStorage.setItem('currentPageNum', `${page + 1}`);
   } else {
     (prev as HTMLElement).style.display = 'none';
   }
-  reRenderData();
+  reRenderData(page, group);
 }
 
-(prev as HTMLElement).addEventListener('click', prevPage);
-(next as HTMLElement).addEventListener('click', nextPage);
-
-reRenderData();
+// page = currentPage;
+// group = currentGroup;
 
 // mongodb+srv://elf888888888:Vtr250_2002@cluster0.9zjrt5z.mongodb.net/?retryWrites=true&w=majority
 
 // https://rs-lang-179.herokuapp.com/doc/
+
+// var reloaded  = function(){...} //страницу перезагрузили
+window.onload = function () {
+  currentGroup = Number(localStorage.getItem('currentGroup'));
+  currentPage = Number(localStorage.getItem('currentPage'));
+  currentPageNum = Number(localStorage.getItem('currentPageNum'));
+  pageNumber.innerHTML = currentPageNum.toString();
+  page = currentPage;
+  reRenderData(currentPage, currentGroup);
+  Array.from(document.getElementsByClassName('group-input')).forEach((element) => {
+    if (Number(element.id) === currentGroup) {
+      element.setAttribute('checked', 'true');
+    }
+  });
+  (prev as HTMLElement).addEventListener('click', () => prevPage(page));
+  (next as HTMLElement).addEventListener('click', () => nextPage(page));
+};
