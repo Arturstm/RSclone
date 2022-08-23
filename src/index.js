a4a9c2e1773799cfbd632c122fc81a27d154207e
Arturstm <96059914+Arturstm@users.noreply.github.com>
2022-08-23 12:04:25 +0400

    feat: add index.js

---
+++ src/index.js
@@ -0,0 +1,5 @@
+function zxc() {
+    document.querySelector('.hamburg-menu').classList.toggle('action');
+    document.querySelector('.hamburg').classList.toggle('action');
+}
+document.querySelector('.hamburg-menu').addEventListener('click', zxc)
