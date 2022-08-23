ba2314176dceb19ac496453865418b189b6a7cef
Arturstm <96059914+Arturstm@users.noreply.github.com>
2022-08-23 12:58:27 +0400

    fix: ts code

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 const hamburgButton = document.querySelector('.hamburg-menu') as HTMLElement;
 const navHamburg = document.querySelector('.hamburg') as HTMLElement;
 
-console.log("Hellow")
-
 hamburgButton.addEventListener('click', (e) => {
     hamburgButton.classList.toggle('action');
     navHamburg.classList.toggle('action');
