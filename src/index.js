6571158d1ece53a6855157d608d9ce5670e68678
Arturstm <96059914+Arturstm@users.noreply.github.com>
2022-08-23 12:34:34 +0400

    feat: delete js code

--- src/index.js
+++
@@ -1,5 +0,0 @@
-function zxc() {
-    document.querySelector('.hamburg-menu').classList.toggle('action');
-    document.querySelector('.hamburg').classList.toggle('action');
-}
-document.querySelector('.hamburg-menu').addEventListener('click', zxc)
