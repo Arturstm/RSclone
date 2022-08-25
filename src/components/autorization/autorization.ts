ae884bd7aa4f3c553d12b49422b9df7625d42672
Elf <Elf@Elf.local>
2022-08-25 20:19:35 +0400

    fix: add conditions for dom elements in autorization.ts

--- src/components/autorization/autorization.ts
+++ src/components/autorization/autorization.ts
@@ -4,14 +4,16 @@ const toRegistration = <HTMLElement>document.querySelector('.toReg');
 const toSingIn = <HTMLElement>document.querySelector('.toLogin');
 const registration = <HTMLElement>document.querySelector('.auth__reg');
 const login = <HTMLElement>document.querySelector('.auth__singin');
+if (toRegistration) {
+  toRegistration.addEventListener('click', () => {
+    registration.classList.add('hide');
+    login.classList.remove('hide');
+  });
+}
 
-toRegistration.addEventListener('click', () => {
-  registration.classList.add('hide');
-  login.classList.remove('hide');
-});
-
-toSingIn.addEventListener('click', () => {
-  login.classList.add('hide');
-  registration.classList.remove('hide');
-});
-console.log('auto');
+if (toSingIn) {
+  toSingIn.addEventListener('click', () => {
+    login.classList.add('hide');
+    registration.classList.remove('hide');
+  });
+}
