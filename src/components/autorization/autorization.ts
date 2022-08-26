ca0585d0517d881a377cafad64bdfa40677892ed
Elf <Elf@Elf.local>
2022-08-26 09:32:18 +0400

    feat: create static page for audio-challange and generate elements from ts

--- src/components/autorization/autorization.ts
+++ src/components/autorization/autorization.ts
@@ -1,3 +1,4 @@
+
 import './autorization.scss';
 
 const loginLayout = `
@@ -308,3 +309,23 @@ class Authorisation {
 const auth = new Authorisation();
 
 auth.openPage();
+
+// import './autorization.css';
+
+// const toRegistration = <HTMLElement>document.querySelector('.toReg');
+// const toSingIn = <HTMLElement>document.querySelector('.toLogin');
+// const registration = <HTMLElement>document.querySelector('.auth__reg');
+// const login = <HTMLElement>document.querySelector('.auth__singin');
+
+// if (toRegistration) {
+//   toRegistration.addEventListener('click', () => {
+//     registration.classList.add('hide');
+//     login.classList.remove('hide');
+//   });
+// }
+// if (toSingIn) {
+//   toSingIn.addEventListener('click', () => {
+//     login.classList.add('hide');
+//     registration.classList.remove('hide');
+//   });
+// }
