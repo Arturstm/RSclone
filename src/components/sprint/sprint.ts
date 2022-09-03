8e75e60c64aaf4afbb4f5c3e744830cb9e8a84a7
Elf <Elf@Elf.local>
2022-09-04 01:19:26 +0400

    fix: some bug in sprint.ts and add the correct and mistakes list

--- src/components/sprint/sprint.ts
+++ src/components/sprint/sprint.ts
@@ -1,7 +1,7 @@
 import '../../index.css';
 import './sprint.css';
-import { fetchData, url, ResponseItem } from '../textbook/textbook';
-import { getRandomItem, shuffle } from '../audio-challange/audio-challange';
+import { fetchData, ResponseItem } from '../textbook/textbook';
+import { getRandomItem } from '../audio-challange/audio-challange';
 
 const startSprint = document.querySelector('#start-sprint');
 const sprintContent = document.querySelector('.sprint-content') as HTMLElement;
@@ -14,11 +14,13 @@ const continueBtn = document.createElement('button');
 const finishBtn = document.createElement('button');
 let counter = 60;
 const counterSpan = document.createElement('span');
+const sprintResultBlock = document.querySelector('.sprint-result') as HTMLElement;
 
 // код утилиты взят отсюда: https://stackoverflow.com/questions/37764665/how-to-implement-sleep-function-in-typescript
 function delay(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
+
 async function getSprintContent(l: number) {
   const currentData: Array<ResponseItem> = [];
   for (let i = 0; i < 30; i++) {
@@ -31,106 +33,121 @@ async function getSprintContent(l: number) {
   sprintContent.innerHTML = '';
   let currentItem = data[0];
   currentItem = data[getRandomItem(data.length)];
-  const counterBlock = document.createElement('div');
-  counterBlock.classList.add('sprint__counter-block');
-  counterBlock.append(counterSpan);
-  sprintContent.append(counterBlock);
-  const sprintWordBlock = document.createElement('div');
-  sprintWordBlock.classList.add('sprint__word-block');
-  sprintContent.append(sprintWordBlock);
-  const sprintWord = document.createElement('p');
-  sprintWord.classList.add('sprint__word');
-  sprintWordBlock.append(sprintWord);
-  const sprintTranslation = document.createElement('p');
-  sprintTranslation.classList.add('sprint__translation');
-  sprintWordBlock.append(sprintTranslation);
-  const sprintResultIcon = document.createElement('i');
-  sprintWordBlock.append(sprintResultIcon);
-  const sprintButtonBlock = document.createElement('div');
-  sprintButtonBlock.classList.add('sprint__button-block');
-  sprintContent.append(sprintButtonBlock);
-  const sprintSbmitBtn = document.createElement('button');
-  sprintSbmitBtn.classList.add('btn');
-  sprintSbmitBtn.classList.add('btn-submit');
-  sprintSbmitBtn.setAttribute('id', 'sprint-submit');
-  sprintSbmitBtn.textContent = 'Правильно';
-  sprintButtonBlock.append(sprintSbmitBtn);
-  const sprintCancelBtn = document.createElement('button');
-  sprintCancelBtn.classList.add('btn');
-  sprintCancelBtn.classList.add('btn-cancel');
-  sprintCancelBtn.setAttribute('id', 'sprint-cancel');
-  sprintCancelBtn.textContent = 'НЕправильно';
-  sprintButtonBlock.append(sprintCancelBtn);
+  if (sprintContent) {
+    const counterBlock = document.createElement('div');
+    counterBlock.classList.add('sprint__counter-block');
+    counterBlock.append(counterSpan);
+    sprintContent.append(counterBlock);
+    const sprintWordBlock = document.createElement('div');
+    sprintWordBlock.classList.add('sprint__word-block');
+    sprintContent.append(sprintWordBlock);
+    const sprintWord = document.createElement('p');
+    sprintWord.classList.add('sprint__word');
+    sprintWordBlock.append(sprintWord);
+    const sprintTranslation = document.createElement('p');
+    sprintTranslation.classList.add('sprint__translation');
+    sprintWordBlock.append(sprintTranslation);
+    const sprintResultIcon = document.createElement('i');
+    sprintWordBlock.append(sprintResultIcon);
+    const sprintButtonBlock = document.createElement('div');
+    sprintButtonBlock.classList.add('sprint__button-block');
+    sprintContent.append(sprintButtonBlock);
+    const sprintSbmitBtn = document.createElement('button');
+    sprintSbmitBtn.classList.add('btn');
+    sprintSbmitBtn.classList.add('btn-submit');
+    sprintSbmitBtn.setAttribute('id', 'sprint-submit');
+    sprintSbmitBtn.textContent = 'Правильно';
+    sprintButtonBlock.append(sprintSbmitBtn);
+    const sprintCancelBtn = document.createElement('button');
+    sprintCancelBtn.classList.add('btn');
+    sprintCancelBtn.classList.add('btn-cancel');
+    sprintCancelBtn.setAttribute('id', 'sprint-cancel');
+    sprintCancelBtn.textContent = 'НЕправильно';
+    sprintButtonBlock.append(sprintCancelBtn);
 
-  const answerVersions: Array<string> = [];
-  answerVersions.push(currentItem.wordTranslate);
-  for (let i = 0; i < 2; i++) {
-    answerVersions.push(data[getRandomItem(data.length)].wordTranslate);
+    const answerVersions: Array<string> = [];
+    answerVersions.push(currentItem.wordTranslate);
+    for (let i = 0; i < 2; i++) {
+      answerVersions.push(data[getRandomItem(data.length)].wordTranslate);
+    }
+
+    sprintWord.textContent = currentItem.word;
+    sprintTranslation.textContent = answerVersions[getRandomItem(answerVersions.length)];
+    sprintButtonBlock.onclick = function (event: Event) {
+      const target = event.target;
+      (document.querySelector('#sprint-submit') as HTMLButtonElement).disabled = true;
+      (document.querySelector('#sprint-cancel') as HTMLButtonElement).disabled = true;
+      let currentMistakes = Number(localStorage.getItem('sprintMistakes'));
+      let currentSprintScore = Number(localStorage.getItem('sprintScore'));
+      if ((target as HTMLButtonElement).id === 'sprint-submit') {
+        if ((sprintTranslation as HTMLElement).textContent === currentItem.wordTranslate) {
+          currentSprintScore++;
+          localStorage.setItem('sprintScore', `${currentSprintScore}`);
+          sprintResultIcon.classList.add('fa-solid');
+          sprintResultIcon.classList.add('fa-check');
+          sprintCorrectWords.push(currentItem.word);
+        } else {
+          currentMistakes++;
+          localStorage.setItem('sprintMistakes', `${currentMistakes}`);
+          sprintResultIcon.classList.add('fa-solid');
+          sprintResultIcon.classList.add('fa-xmark');
+          sprintMistakes.push(currentItem.word);
+        }
+        getSprintContent(level);
+      }
+      if ((target as HTMLButtonElement).id === 'sprint-cancel') {
+        if ((sprintTranslation as HTMLElement).textContent !== currentItem.wordTranslate) {
+          currentSprintScore++;
+          localStorage.setItem('sprintScore', `${currentSprintScore}`);
+          sprintResultIcon.classList.add('fa-solid');
+          sprintResultIcon.classList.add('fa-check');
+          sprintCorrectWords.push(currentItem.word);
+        } else {
+          currentMistakes++;
+          localStorage.setItem('sprintMistakes', `${currentMistakes}`);
+          sprintResultIcon.classList.add('fa-solid');
+          sprintResultIcon.classList.add('fa-xmark');
+          sprintMistakes.push(currentItem.word);
+        }
+        getSprintContent(level);
+      }
+    };
   }
-
-  sprintWord.textContent = currentItem.word;
-  sprintTranslation.textContent = answerVersions[getRandomItem(answerVersions.length)];
-  sprintButtonBlock.onclick = function (event: Event) {
-    const target = event.target;
-    (document.querySelector('#sprint-submit') as HTMLButtonElement).disabled = true;
-    (document.querySelector('#sprint-cancel') as HTMLButtonElement).disabled = true;
-    let currentMistakes = Number(localStorage.getItem('sprintMistakes'));
-    let currentSprintScore = Number(localStorage.getItem('sprintScore'));
-    if ((target as HTMLButtonElement).id === 'sprint-submit') {
-      if ((sprintTranslation as HTMLElement).textContent === currentItem.wordTranslate) {
-        currentSprintScore++;
-        localStorage.setItem('sprintScore', `${currentSprintScore}`);
-
-        sprintResultIcon.classList.add('fa-solid');
-        sprintResultIcon.classList.add('fa-check');
-      } else {
-        currentMistakes++;
-        localStorage.setItem('sprintMistakes', `${currentMistakes}`);
-        sprintResultIcon.classList.add('fa-solid');
-        sprintResultIcon.classList.add('fa-xmark');
-      }
-      getSprintContent(level);
-    }
-    if ((target as HTMLButtonElement).id === 'sprint-cancel') {
-      if ((sprintTranslation as HTMLElement).textContent !== currentItem.wordTranslate) {
-        currentSprintScore++;
-        localStorage.setItem('sprintScore', `${currentSprintScore}`);
-        sprintResultIcon.classList.add('fa-solid');
-        sprintResultIcon.classList.add('fa-check');
-      } else {
-        currentMistakes++;
-        localStorage.setItem('sprintMistakes', `${currentMistakes}`);
-        sprintResultIcon.classList.add('fa-solid');
-        sprintResultIcon.classList.add('fa-xmark');
-      }
-      getSprintContent(level);
-    }
-  };
-
-  return;
 }
 
 function shouResults() {
-  sprintContent.innerHTML = '';
-  const resultBlock = document.createElement('div');
-  resultBlock.classList.add('sprint__result-block');
+  sprintContent.style.display = 'none';
+  sprintResultBlock.style.display = '';
+  sprintResultBlock.classList.add('sprint__result-block');
   const resultMistakes = document.createElement('p');
   resultMistakes.classList.add('sprint__result-mistakes');
   resultMistakes.textContent = `Ошибок: ${localStorage.getItem('sprintMistakes')}`;
-  resultBlock.append(resultMistakes);
+  sprintResultBlock.append(resultMistakes);
   const mistakesList = document.createElement('ul');
   mistakesList.classList.add('sprint__mistakes-list');
-  resultBlock.append(mistakesList);
+  sprintResultBlock.append(mistakesList);
+  sprintMistakes.forEach((word) => {
+    const mistakesListItem = document.createElement('li');
+    mistakesListItem.classList.add('mistakes-list__item');
+    mistakesListItem.textContent = word;
+    mistakesList.append(mistakesListItem);
+  });
   const resultRight = document.createElement('p');
   resultRight.classList.add('sprint__result-right');
   resultRight.textContent = `Правильных ответов: ${localStorage.getItem('sprintScore')}`;
-  resultBlock.append(resultRight);
+  sprintResultBlock.append(resultRight);
   const correctWordsList = document.createElement('ul');
   correctWordsList.classList.add('sprint__correct-words');
-  resultBlock.append(correctWordsList);
+  sprintResultBlock.append(correctWordsList);
+  sprintCorrectWords.forEach((word) => {
+    const correctWordsListItem = document.createElement('li');
+    correctWordsListItem.classList.add('correct-list__item');
+    correctWordsListItem.textContent = word;
+    correctWordsList.append(correctWordsListItem);
+  });
   const resultBtnBlock = document.createElement('div');
-  resultBlock.classList.add('sprint__result-buttons');
-  resultBlock.append(resultBtnBlock);
+  sprintResultBlock.classList.add('sprint__result-buttons');
+  sprintResultBlock.append(resultBtnBlock);
   continueBtn.classList.add('btn');
   continueBtn.classList.add('btn-submit');
   continueBtn.textContent = 'Ещё раз';
@@ -139,7 +156,7 @@ function shouResults() {
   finishBtn.classList.add('btn-cancel');
   finishBtn.textContent = 'Закончить игру';
   resultBtnBlock.append(finishBtn);
-  sprintContent.append(resultBlock);
+  (document.querySelector('.sprint-container') as HTMLElement).append(sprintResultBlock);
   localStorage.removeItem('sprintScore');
   localStorage.removeItem('sprintMistakes');
   sprintMistakes = [];
@@ -154,7 +171,6 @@ async function countTime() {
   }
 }
 
-// код взят отсюда: https://qna.habr.com/q/1168740
 async function sprintTiming(l: number) {
   await getSprintContent(l);
   await delay(61000);
@@ -169,6 +185,7 @@ if (startSprint) {
         level = Number(input.id);
       }
     });
+    sprintContent.style.display = '';
     sprintTiming(level);
     countTime();
   });
@@ -177,7 +194,10 @@ if (startSprint) {
 if (continueBtn) {
   continueBtn.addEventListener('click', (e: Event) => {
     (continueBtn as HTMLButtonElement).disabled = true;
+    sprintContent.style.display = '';
     sprintTiming(level);
+    sprintContent.style.display = '';
+    sprintResultBlock.style.display = 'none';
     counter = 60;
     countTime();
   });
