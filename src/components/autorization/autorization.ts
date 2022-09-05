5769cc452a10599e72375a7ac99a840fd08416cd
Elf <Elf@Elf.local>
2022-09-05 13:20:04 +0400

    fix: migrate autorization to ts and handle errors

--- src/components/autorization/autorization.ts
+++ src/components/autorization/autorization.ts
@@ -1,6 +1,117 @@
-// @ts-nocheck
-import './autorization.css';
+// // @ts-nocheck
+// import './autorization.css';
 
+// const toRegistration = document.querySelector('.toLogin');
+// const toSingIn = document.querySelector('.toReg');
+// const registration = document.querySelector('.auth__reg');
+// const login = document.querySelector('.auth__singin');
+
+// const baseUrl = 'https://rs-lang-179.herokuapp.com';
+
+// toRegistration.addEventListener('mousedown', () => {
+//   registration.classList.add('hide');
+//   login.classList.remove('hide');
+// });
+
+// toSingIn.addEventListener('mousedown', () => {
+//   login.classList.add('hide');
+//   registration.classList.remove('hide');
+// });
+
+// const authForm = document.getElementById('auth__form');
+// const logForm = document.getElementById('log__form');
+
+// const authUserName = authForm.querySelector('#auth_userName');
+// const authEmail = authForm.querySelector('#auth_email');
+// const authPass = authForm.querySelector('#auth_pass');
+
+// const logEmail = logForm.querySelector('#log_email');
+// const logPass = logForm.querySelector('#log_pass');
+
+// const createUser = async (user) => {
+//   const rawResponse = await fetch('https://rs-lang-179.herokuapp.com/users', {
+//     method: 'POST',
+//     headers: {
+//       Accept: 'application/json',
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(user),
+//   });
+//   const content = await rawResponse.json();
+
+//   console.log(content);
+//   return `${rawResponse.status}`;
+// };
+
+// const regUser = async (event) => {
+//   event.preventDefault();
+//   const resStatus = await createUser({
+//     name: authUserName.value,
+//     email: authEmail.value,
+//     password: authPass.value,
+//   });
+
+//   if (resStatus === '200') {
+//     console.log('Зарегистрирован');
+//   } else {
+//     console.log(`${resStatus}`);
+//   }
+// };
+
+// const loginUser = async (user) => {
+//   const rawResponse = await fetch('https://rs-lang-179.herokuapp.com/signin', {
+//     method: 'POST',
+//     headers: {
+//       Accept: 'application/json',
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(user),
+//   });
+
+//   if (rawResponse.status === 200) {
+//     const content = await rawResponse.json();
+//     return content;
+//   } else {
+//     return rawResponse.status;
+//   }
+// };
+
+// const signIn = async (e) => {
+//   e.preventDefault();
+//   const res = await loginUser({
+//     email: logEmail.value,
+//     password: logPass.value,
+//   });
+
+//   if (typeof res === 'number') {
+//     console.log(`Error ${res}`);
+//   } else {
+//     const signInEvent = new Event('signin');
+//     document.dispatchEvent(signInEvent);
+
+//     localStorage.setItem('token', res.token);
+//     localStorage.setItem('refreshToken', res.refreshToken);
+//     localStorage.setItem('userID', res.userId);
+//     localStorage.setItem('userName', res.name);
+//     console.log(res);
+//   }
+// };
+
+// authForm.addEventListener('submit', regUser);
+// logForm.addEventListener('submit', signIn);
+
+import './autorization.css';
+interface NewUser {
+  name: string;
+  email: string;
+  password: string;
+}
+interface OldUser {
+  email: string;
+  password: string;
+}
+// regexp взят отсюда https://ru.hexlet.io/blog/posts/validatsiya-email-na-javascript
+const emailRegexp = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
 const toRegistration = document.querySelector('.toLogin');
 const toSingIn = document.querySelector('.toReg');
 const registration = document.querySelector('.auth__reg');
@@ -8,57 +119,70 @@ const login = document.querySelector('.auth__singin');
 
 const baseUrl = 'https://rs-lang-179.herokuapp.com';
 
-toRegistration.addEventListener('mousedown', () => {
-  registration.classList.add('hide');
-  login.classList.remove('hide');
-});
-
-toSingIn.addEventListener('mousedown', () => {
-  login.classList.add('hide');
-  registration.classList.remove('hide');
-});
-
+if (toRegistration) {
+  (toRegistration as HTMLElement).addEventListener('mousedown', () => {
+    (registration as HTMLElement).classList.add('hide');
+    (login as HTMLElement).classList.remove('hide');
+  });
+}
+if (toSingIn) {
+  (toSingIn as HTMLElement).addEventListener('mousedown', () => {
+    (login as HTMLElement).classList.add('hide');
+    (registration as HTMLElement).classList.remove('hide');
+  });
+}
 const authForm = document.getElementById('auth__form');
 const logForm = document.getElementById('log__form');
 
-const authUserName = authForm.querySelector('#auth_userName');
-const authEmail = authForm.querySelector('#auth_email');
-const authPass = authForm.querySelector('#auth_pass');
+const authUserName = document.querySelector('#auth_userName');
+const authEmail = document.querySelector('#auth_email');
+const authPass = document.querySelector('#auth_pass');
 
-const logEmail = logForm.querySelector('#log_email');
-const logPass = logForm.querySelector('#log_pass');
+const logEmail = document.querySelector('#log_email');
+const logPass = document.querySelector('#log_pass');
 
-const createUser = async (user) => {
-  const rawResponse = await fetch('https://rs-lang-179.herokuapp.com/users', {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(user),
-  });
-  const content = await rawResponse.json();
-
-  console.log(content);
-  return `${rawResponse.status}`;
+const createUser = async (user: NewUser) => {
+  try {
+    const rawResponse = await fetch('https://rs-lang-179.herokuapp.com/users', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    });
+    const content = await rawResponse.json();
+    console.log(content);
+    return `${rawResponse.status}`;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-const regUser = async (event) => {
+const regUser = async (event: Event) => {
   event.preventDefault();
   const resStatus = await createUser({
-    name: authUserName.value,
-    email: authEmail.value,
-    password: authPass.value,
+    name: (authUserName as HTMLFormElement).value,
+    email: (authEmail as HTMLFormElement).value,
+    password: (authPass as HTMLFormElement).value,
   });
-
   if (resStatus === '200') {
     console.log('Зарегистрирован');
+    alert('Поздравляем! Вы зарегестрированы на LangTrip.');
+    (registration as HTMLElement).classList.add('hide');
+    (login as HTMLElement).classList.remove('hide');
+  } else if (!emailRegexp.test((authEmail as HTMLImageElement).textContent as string)) {
+    alert('Введите валидный email');
+    console.log(emailRegexp.test((authEmail as HTMLImageElement).textContent as string));
+  } else if ((authPass as HTMLInputElement).value.length < 8) {
+    alert('Пароль должен быть минимум 8 символов длинной');
   } else {
+    alert('Пользователь уже зарегестрирован. Войдите или введите другие данные для регистрации');
     console.log(`${resStatus}`);
   }
 };
 
-const loginUser = async (user) => {
+const loginUser = async (user: OldUser) => {
   const rawResponse = await fetch('https://rs-lang-179.herokuapp.com/signin', {
     method: 'POST',
     headers: {
@@ -76,15 +200,16 @@ const loginUser = async (user) => {
   }
 };
 
-const signIn = async (e) => {
+const signIn = async (e: Event) => {
   e.preventDefault();
   const res = await loginUser({
-    email: logEmail.value,
-    password: logPass.value,
+    email: (logEmail as HTMLFormElement).value,
+    password: (logPass as HTMLFormElement).value,
   });
 
   if (typeof res === 'number') {
     console.log(`Error ${res}`);
+    alert('Одно из полей заполнено неверно. Попробуйте ещё раз.');
   } else {
     const signInEvent = new Event('signin');
     document.dispatchEvent(signInEvent);
@@ -93,9 +218,14 @@ const signIn = async (e) => {
     localStorage.setItem('refreshToken', res.refreshToken);
     localStorage.setItem('userID', res.userId);
     localStorage.setItem('userName', res.name);
+    window.location.href = './index.html';
     console.log(res);
   }
 };
 
-authForm.addEventListener('submit', regUser);
-logForm.addEventListener('submit', signIn);
+if (authForm) {
+  authForm.addEventListener('submit', regUser);
+}
+if (logForm) {
+  logForm.addEventListener('submit', signIn);
+}
